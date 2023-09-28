69b73e30abeac6b909216b9a3907750084a56af6
renegade1912 <marcosendner@web.de>
2023-09-28 17:11:59 +0200

    own footer component

---
+++ components/layout/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+    <q-footer class="bg-transparent q-px-md q-pb-sm">
+        <div class="text-subtitle2">
+            COPYRIGHT © 2022 - {{ new Date().getFullYear() }}
+            <a href="https://discord.unlimited.wtf" target="_blank" style="text-decoration: none"
+                ><span class="text-unlimited">Unlimited</span></a
+            >, All rights Reserved
+        </div>
+    </q-footer>
+</template>
