67597c3f99386023198a3af41e19c5a22399146c
Renegade <renegade@unlimited.wtf>
2023-11-08 02:49:43 +0100

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@ import { fileURLToPath } from 'url';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    nitro: {
-        preset: 'cloudflare'
-    },
     devtools: { enabled: true },
     app: {
         pageTransition: { name: 'page', mode: 'out-in' },
