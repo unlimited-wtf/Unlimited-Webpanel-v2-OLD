67493a04bb0f3d1d1e05e862c35e4010101ed594
Renegade <renegade@unlimited.wtf>
2024-01-01 01:11:13 +0100

    remove transition

--- app.vue
+++ app.vue
@@ -13,16 +13,6 @@ useHead({
 </script>
 
 <style>
-.page-enter-active,
-.page-leave-active {
-    transition: all 0.4s;
-}
-.page-enter-from,
-.page-leave-to {
-    opacity: 0;
-    filter: blur(1rem);
-}
-
 .noscroll {
     height: 100%;
     overflow: hidden;
