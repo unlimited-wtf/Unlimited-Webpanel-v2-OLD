bc5d7b97fe1d0304c8795df7f031c0132125bef2
Renegade <renegade@unlimited.wtf>
2024-01-01 01:11:28 +0100

    start custom error pages

---
+++ components/error/404.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="absolute-center column items-center">
+        <h3 class="text-weight-medium q-mb-md">404</h3>
+        <strong class="text-h5">{{ $t('error.pageNotFound') }} ⚠️</strong>
+        <div class="q-my-md text-subtitle1 text-center">{{ $t('error.pageNotFoundText') }}</div>
+        <q-btn no-caps class="q-mt-md" color="primary" @click="handleError" :label="$t('error.back')" />
+    </div>
+</template>
+
+<script setup>
+const handleError = () => clearError({ redirect: '/ucp' });
+</script>
---
+++ error.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <component :is="resolverErrorComponent" />
+        <ImagesBackgroundShape />
+    </div>
+</template>
+
+<script setup>
+const error = useError();
+
+const Error404 = resolveComponent('Error404');
+
+const resolverErrorComponent = computed(() => {
+    switch (error.value?.statusCode) {
+        case 404:
+            return Error404;
+        default:
+            return Error404;
+    }
+});
+
+
+</script>
