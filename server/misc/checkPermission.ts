910a7040aebaeee5367a52b4a40a6de01ca67730
Renegade <renegade@unlimited.wtf>
2024-01-01 01:48:10 +0100

    update types

--- server/misc/checkPermission.ts
+++ server/misc/checkPermission.ts
@@ -1,10 +1,10 @@
-import { actions, subjects } from '~/shared/permissions';
+import { Actions, Subjects } from '~/shared/permissions';
 import MYSQL from '../database';
 
 export const checkPermission = async (
     discordId: string,
-    action: typeof actions,
-    subject: typeof subjects
+    action: Actions,
+    subject: Subjects
 ): Promise<boolean> => {
     const results: any[] = await MYSQL.query(
         `SELECT 1 AS 'can' FROM unl_user_roles, unl_role_permissions, unl_permissions WHERE unl_user_roles.discordId = ? AND unl_role_permissions.roleId = unl_user_roles.roleId AND unl_permissions.action = ? AND unl_permissions.subject = ? LIMIT 1`,
