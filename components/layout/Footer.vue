d0489e2151495cd860ffdb2c04d47900b47211be
Renegade <renegade@unlimited.wtf>
2024-01-02 02:19:20 +0100

    copyright

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,6 +1,17 @@
 <template>
     <q-footer class="bg-transparent q-px-md q-pb-sm">
         <div class="text-subtitle2">
+            <!-- 
+                !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+                Don´t be a fool! Do not remove this copyright notice.
+                You are not allowed to remove it.
+                If you do, you will be in violation of the license and no support will be provided.
+                Otherwise the panel may no longer be open source.
+
+                !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+             -->
+
             COPYRIGHT © 2022 - {{ new Date().getFullYear() }}
             <a href="https://discord.unlimited.wtf" target="_blank" style="text-decoration: none"
                 ><span class="text-unlimited">Unlimited</span></a
