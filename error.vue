e5e7db4a21c2140ad98c17d80bc0d47076fb068a
Renegade <renegade@unlimited.wtf>
2024-01-03 02:08:27 +0100

    add 401

--- error.vue
+++ error.vue
@@ -8,10 +8,14 @@
 <script setup>
 const error = useError();
 
+const Error401 = resolveComponent('Error401');
 const Error404 = resolveComponent('Error404');
 
 const resolverErrorComponent = computed(() => {
+    console.log(error.value)
     switch (error.value?.statusCode) {
+        case 401:
+            return Error401;
         case 404:
             return Error404;
         default:
