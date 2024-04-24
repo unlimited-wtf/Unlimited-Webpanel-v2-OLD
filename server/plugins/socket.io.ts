ca537933d484fa87677f72be9817cd03d5049f66
Renegade <renegade@unlimited.wtf>
2024-04-24 17:37:24 +0200

    auth

--- server/plugins/socket.io.ts
+++ server/plugins/socket.io.ts
@@ -2,6 +2,15 @@ import type { NitroApp } from 'nitropack';
 import { Server as Engine } from 'engine.io';
 import { Server, type ServerOptions } from 'socket.io';
 import { defineEventHandler } from 'h3';
+import { IncomingMessage, ServerResponse } from 'http';
+import { H3Event } from 'h3';
+import { getServerSession } from '#auth';
+import { UserSession } from '../api/auth/[...]';
+
+interface ExtendedIncomingMessage extends IncomingMessage {
+    nitroEvent: H3Event;
+    _query: { sid: string };
+}
 
 const options: Partial<ServerOptions> = {
     path: '/socket.io',
@@ -13,6 +22,27 @@ export const io = new Server();
 
 export default defineNitroPlugin((nitroApp: NitroApp) => {
     io.bind(engine);
+
+    // SocketIO Auth middleware
+    io.engine.use(async (req: ExtendedIncomingMessage, res: ServerResponse, next: (err?: Error) => void) => {
+        const isHandshake = req._query.sid === undefined;
+
+        // Skip if it's not a handshake
+        if (!isHandshake) {
+            return next();
+        }
+
+        // Check if the user is authenticated on the handshake
+        const session: UserSession | null = await getServerSession(req.nitroEvent)
+        if (!session || !session.user) {
+            // Unauthorized
+            return next(new Error('Unauthorized'));
+        }
+
+        // Authorized
+        next();
+    });
+
     io.on('connection', async (socket) => {
         console.log('A user connected');
 
@@ -25,7 +55,11 @@ export default defineNitroPlugin((nitroApp: NitroApp) => {
         '/socket.io/',
         defineEventHandler({
             handler(event) {
-                engine.handleRequest(event.node.req, event.node.res);
+                // Add the Nitro event to the request object for getServerSession
+                const req = event.node.req as ExtendedIncomingMessage;
+                req.nitroEvent = event;
+
+                engine.handleRequest(req, event.node.res);
                 event._handled = true;
             },
             websocket: {
