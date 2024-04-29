8691e51f75432cb7443670f6c8e3e1fd9e686b33
Renegade <renegade@unlimited.wtf>
2024-04-29 20:19:34 +0200

    shorter return

--- server/api/status.get.ts
+++ server/api/status.get.ts
@@ -1,6 +1,4 @@
 import { ServerStatus } from '../cron/server.status';
 
 // Cache return the server status from the "cache" instead of the FiveM-Server
-export default defineEventHandler(() => {
-    return ServerStatus;
-});
+export default defineEventHandler(() => ServerStatus);
