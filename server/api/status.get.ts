080dbb983d19e69ad7052b6c0fa849c5bb8dec19
Renegade <renegade@unlimited.wtf>
2024-04-29 19:52:52 +0200

    decrease cache time

--- server/api/status.get.ts
+++ server/api/status.get.ts
@@ -1,9 +1,9 @@
 import { ServerStatus } from '../cron/server.status';
 
-// Cache the server status for 2 minutes and return it from the "cache" instead of the FiveM-Server
+// Cache the server status for 1 minute and return it from the "cache" instead of the FiveM-Server
 export default defineCachedEventHandler(
     () => {
         return ServerStatus;
     },
-    { maxAge: 60 * 2 /* 2 minute */ }
+    { maxAge: 60 /* 1 minute */ }
 );
