64422c6f56cb8d7bf9ab69b610b5c8fb1be9cf96
Renegade <renegade@unlimited.wtf>
2024-04-30 09:46:14 +0200

    cleanup

--- server/cron/server.status.ts
+++ server/cron/server.status.ts
@@ -1,11 +1,9 @@
 import { defineCronHandler } from '#nuxt/cron';
-import { Server } from 'http';
 import { useLogger } from '~/server/utils/useLogger';
 import { GameServerStatus } from '~/shared/types/game';
 import { io } from '../plugins/socket.io';
-import { log } from 'console';
 
-const { logServerInfo } = useLogger();
+const { logDebugServerInfo } = useLogger();
 
 type GameServerResponse = {
     clients: number;
@@ -24,7 +22,7 @@ export const ServerStatus: GameServerStatus = {
 export default defineCronHandler(
     'everyMinute',
     async () => {
-        logServerInfo('Checking Server Status ...');
+        logDebugServerInfo('Checking Server Status ...');
 
         try {
             // Check if server is online
@@ -40,13 +38,13 @@ export default defineCronHandler(
             ServerStatus.players = data.clients;
             ServerStatus.maxPlayers = Number(data.sv_maxclients);
 
-            logServerInfo(
+            logDebugServerInfo(
                 `Server Status: ${ServerStatus.status} - Players: ${ServerStatus.players} / ${ServerStatus.maxPlayers}`
             );
 
             // Update server status on clients if changed
             if (tmpStatus !== ServerStatus.status) {
-                logServerInfo('Server Status changed - Update Clients ...');
+                logDebugServerInfo('Server Status changed - Update Clients ...');
 
                 // Emit server status to clients
                 io.emit('update:server:status', ServerStatus);
@@ -57,7 +55,7 @@ export default defineCronHandler(
             ServerStatus.players = 0;
             ServerStatus.maxPlayers = 32;
 
-            logServerInfo(`Server Status: ${ServerStatus.status}`);
+            logDebugServerInfo(`Server Status: ${ServerStatus.status}`);
         }
     },
     { runOnInit: true }
