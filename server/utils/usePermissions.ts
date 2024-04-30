d252e3da60e2a4c1cd3934591c7936622903da93
Renegade <renegade@unlimited.wtf>
2024-04-30 10:16:17 +0200

    types

--- server/utils/usePermissions.ts
+++ server/utils/usePermissions.ts
@@ -2,7 +2,7 @@ import { Subjects, Actions } from '~/shared/permissions';
 import MYSQL from '../db';
 
 export const usePermissions = () => {
-    const getPermissions = async (discordId: string) => {
+    const getPermissions = async (discordId: string): Promise<Array<{ subject: Subjects; action: Actions }>> => {
         const data: { permissions: Array<{ subject: Subjects; action: Actions }> }[] = await MYSQL.query(
             `SELECT permissions FROM unl_roles, unl_user_roles, unl_users WHERE unl_users.discordId = ? AND unl_roles.id = unl_user_roles.roleId`,
             [discordId]
@@ -11,11 +11,11 @@ export const usePermissions = () => {
         return data[0] ? JSON.parse(data[0]?.permissions as unknown as string) : [];
     };
 
-    const hasPermission = async (
+    const hasPermission = (
         permissions: Array<{ subject: Subjects; action: Actions }>,
         subject: Subjects,
         action: Actions
-    ) => {
+    ): boolean => {
         return permissions.some((permission) => permission.subject === subject && permission.action === action);
     };
 
